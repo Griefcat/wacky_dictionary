51c90712e6ccd82ca41ac26e75a0135033b5d7ae
Griefcat <anardolilli@gmail.com>
2020-10-30 17:38:43 -0400

    almost done

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -11,4 +11,37 @@ class WordsController < ApplicationController
         render :show
     end 
 
+    def new
+        @word = Word.new
+        render :new
+    end 
+
+    def create
+        new_word = params[:word][:word]
+        new_definition = params[:word][:definition]
+        new_part_of_speech = params[:word][:part_of_speech]
+        new_example_sentence = params[:word][:example_sentence]
+
+        whole_new_word = Word.create(word: new_word, definition: new_definition, part_of_speech: new_part_of_speech, example_sentence: new_example_sentence)
+
+        redirect_to words_path
+        
+    end 
+
+    def edit 
+        @word = Word.find(params[:id])
+        render :edit 
+    end 
+
+    def update
+        @word = Word.find(params[:id])
+        @word.update(our_params)
+        redirect_to words_path
+
+    end 
+
+    private
+    def our_params
+        params.require(:word).permit(:word, :definition, :part_of_speech, :example_sentence)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 
   get "/", to: "static#home"
   get "/about", to: "static#about"
+
   get "/words", to: "words#index"
+  get "/words/new", to: "words#new"
   get "/words/:id", to: "words#show", as: "word"
+  post "/words/", to: "words#create", as: "new_word"
+  get "/words/:id/edit", to: "words#edit", as: "edit_word"
+  patch "/words/:id", to: "words#update", as: "update_word"
+  
 
 
 
